c01a159951d7771631951f5e36847bf89e717c2a
Imasha <Imasha@SILVERBUNNY>
2024-05-05 23:08:45 +0530

    Add project files.

---
+++ Lab 02 Q1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_Q1
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Book myBook = new Book();
+
+            
+            myBook.Title = "Harry Potter";
+            myBook.Author = "J.K.Rowling";
+
+            
+            Console.WriteLine("Book Title: " + myBook.Title);
+            Console.WriteLine("Book Author: " + myBook.Author);
+
+            Console.ReadLine(); 
+        }
+    }
+}
+
+
+      
+
